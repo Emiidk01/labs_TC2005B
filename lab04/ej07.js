7e633cb6c59c32cefee0931409520e3d75095641
Emiliano Gomez <a01710711@tec.mx>
2024-04-07 22:20:11 -0600

    Add javascript questions

---
+++ lab04/ej07.js
@@ -0,0 +1,34 @@
+// Diferencias y semejanzas entre Java y JavaScript
+console.log("Diferencias y semejanzas entre Java y JavaScript:");
+console.log("- Diferencias:");
+console.log("  - Java es un lenguaje de programación orientado a objetos, mientras que JavaScript es un lenguaje de programación interpretado y basado en prototipos.");
+console.log("  - Java es compilado antes de su ejecución, mientras que JavaScript es interpretado en tiempo de ejecución por el navegador o el entorno de ejecución.");
+console.log("  - Java es utilizado principalmente en el desarrollo de aplicaciones de servidor y de escritorio, mientras que JavaScript es utilizado principalmente para desarrollo web y del lado del cliente.");
+console.log("- Semejanzas:");
+console.log("  - Ambos tienen una sintaxis similar, lo que facilita la transición entre los dos para los desarrolladores.");
+console.log("  - Ambos son lenguajes de programación multiparadigma que admiten la programación orientada a objetos y funcional.");
+
+// Metodos del objeto Date
+console.log("\n¿Qué métodos tiene el objeto Date? (Menciona al menos 5)");
+const date = new Date();
+console.log("date.getDate():", date.getDate());
+console.log("date.getMonth():", date.getMonth());
+console.log("date.getFullYear():", date.getFullYear());
+console.log("date.getHours():", date.getHours());
+console.log("date.getMinutes():", date.getMinutes());
+
+// Metodos de los arreglos
+console.log("\n¿Qué métodos tienen los arreglos? (Menciona al menos 5)");
+console.log("array.length: Devuleve el tamaño del arreglo");
+console.log("array.push(): Agrega un elemento al final del arreglo y devuelve la nueva longitud");
+console.log("array.pop(): Elimina el último elemento del arreglo y lo devuelve"); 
+console.log("array.join(', '): Une todos los elementos del arreglo en una cadena separada por el separador especificado");
+console.log("array.includes(): Determina si el arreglo contiene un elemento especificado");
+
+// Variable con alcance local dentro de una función
+console.log("\n¿Cómo se declara una variable con alcance local dentro de una función?");
+console.log("Simplemente puedes crear la variable dentro de una función y utilizarla adentro")
+
+// Implicaciones de utilizar variables globales dentro de funciones
+console.log("\n¿Qué implicaciones tiene utilizar variables globales dentro de funciones?");
+console.log("Puedes utilizarlas afuera y adentro de una funcion");
